6dcbab6e01df5a684d7154cf0454da5e9f8438db
igorevanss <igorevans.pro@gmail.com>
2022-07-20 14:05:10 -0300

    implementação de home screen

---
+++ src/components/home.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="home">
+      <img src="./images/logo.png" alt=""/>
+
+      <h2>ZapRecall</h2>
+      
+      <button><p>Iniciar Recall!</p></button>
+    </div>
+  )
+}
