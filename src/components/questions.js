75e14507eab8aad07e3dcf5a5b097285986390a3
igorevanss <igorevans.pro@gmail.com>
2022-07-25 17:34:43 -0300

    Commit de até onde eu consegui fazer

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,46 +1,39 @@
-import React from "react"
-import Footer from "./footer"
-import Header from "./header"
+import React from 'react'
+import Footer from './footer'
+import Header from './header'
 
 export default function Questions() {
-  const [question, setQuestion]=React.useState("flash-card")
-  const [answer, setAnswer]=React.useState("answer")
+  const [question, setQuestion] = React.useState('flash-card')
 
   return (
-    
     <div className="questions">
       <Header />
       <div div className="deck">
-        <div className={question}
-        onClick={() =>{
-          setQuestion("question")
-        }}>
-          <p>Pergunta 1</p>
+        <div
+          className={question}
+          onClick={() => {
+            setQuestion('question')
+          }}
+        >
+          <strong>Pergunta 1</strong>
+
+          <p /*className="reply"*/>
+            JSX é uma sintaxe para escrever HTML dentro do JS
+          </p>
+
           <span>O que é JSX?</span>
+
           <ion-icon name="play-outline"></ion-icon>
-          <img src="./images/setinha.png" />
-        </div>
-        <div className={question}>
-          <p>Pergunta 2</p> 
-          <span>O que é JSX?</span>
-          <ion-icon name="play-outline"></ion-icon>
-          <img src="./images/setinha.png" />
-        </div>
-        <div className={question}>
-          <p>Pergunta 3</p>
-          <span>O que é JSX?</span>
-          <ion-icon name="play-outline"></ion-icon>
-          <img src="./images/setinha.png" />
-        </div>
-        <div className={question}>
-          <p>Pergunta 4</p>
-          <span>O que é JSX?</span>
-          <ion-icon name="play-outline"></ion-icon>
-          <img src="./images/setinha.png" />
+
+          <img
+            onClick={() => {
+              setQuestion('answer')
+            }}
+            src="./images/setinha.png"
+          />
         </div>
       </div>
       <Footer />
     </div>
-    
   )
 }
